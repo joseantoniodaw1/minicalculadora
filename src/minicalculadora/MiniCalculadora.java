2dd892f7d8d44de5d4433f78284671759f76867c
alumno <alumno@192.168.4.14>
2015-05-04 13:11:44 +0200

    segunda versión del programa. Funciona bien. posibles mejoras...

--- src/minicalculadora/MiniCalculadora.java
+++ src/minicalculadora/MiniCalculadora.java
@@ -29,12 +29,12 @@ public class MiniCalculadora extends javax.swing.JFrame {
 
         txfNum1 = new javax.swing.JTextField();
         txfNum2 = new javax.swing.JTextField();
-        txFResultado = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox();
-        jButton1 = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        txfResultado = new javax.swing.JTextField();
+        comboBox = new javax.swing.JComboBox();
+        boton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("MINICALCULADORA");
 
         txfNum1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         txfNum1.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
@@ -44,18 +44,20 @@ public class MiniCalculadora extends javax.swing.JFrame {
         txfNum2.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
         txfNum2.setText("0");
 
-        txFResultado.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        txFResultado.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
-        txFResultado.setText("0");
+        txfResultado.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        txfResultado.setHorizontalAlignment(javax.swing.JTextField.RIGHT);
+        txfResultado.setText("0");
 
-        jComboBox1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "+", "-", "*", "/" }));
+        comboBox.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        comboBox.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "+", "-", "*", "/" }));
 
-        jButton1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jButton1.setText("=");
-
-        jLabel1.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
-        jLabel1.setText("MINICALCULADORA");
+        boton.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
+        boton.setText("=");
+        boton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                calcular(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -63,38 +65,72 @@ public class MiniCalculadora extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(txfNum1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txfNum2, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jButton1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txFResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addComponent(txfNum1, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(comboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txfNum2, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(boton)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txfResultado, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(20, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addComponent(jLabel1)
-                .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                     .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                         .addComponent(txfNum2)
-                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(txFResultado))
-                    .addComponent(jComboBox1)
+                        .addComponent(boton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(txfResultado))
+                    .addComponent(comboBox)
                     .addComponent(txfNum1))
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void calcular(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_calcular
+   Numero n1 = new Numero(txfNum1.getText(), ',');
+ Numero n2 = new Numero(txfNum2.getText(), ',');
+ double num1 = n1.getNumero();
+ double num2 = n2.getNumero();
+ double resul = 0;
+ if (num1 == Double.MAX_VALUE || num2 == Double.MAX_VALUE) {
+ txfResultado.setText("error");
+ } else {
+ String operacion = (String) comboBox.getSelectedItem();
+ switch (operacion) {
+ case "+":
+ resul = num1 + num2;
+ break;
+ case "-":
+ resul = num1 - num2;
+ break;
+ case "*":
+ resul = num1 * num2;
+ break;
+ case "/":
+ if(num2==0){
+ resul=Double.MAX_VALUE;
+ }else{
+ resul = num1 / num2;
+ }
+ }
+ if(resul==Double.MAX_VALUE){
+ txfResultado.setText("error");
+
+ }else{
+ Numero resultado=new Numero(resul);
+ txfResultado.setText(resultado.toString());
+ }
+ }
+ 
+
+    }//GEN-LAST:event_calcular
 
     /**
      * @param args the command line arguments
@@ -132,11 +168,10 @@ public class MiniCalculadora extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox jComboBox1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField txFResultado;
+    private javax.swing.JButton boton;
+    private javax.swing.JComboBox comboBox;
     private javax.swing.JTextField txfNum1;
     private javax.swing.JTextField txfNum2;
+    private javax.swing.JTextField txfResultado;
     // End of variables declaration//GEN-END:variables
 }
